e589b2bae9ee188510a0f8bb94ee35ee0aed997e
Javier G <javier.guajardoj@gmail.com>
2022-01-14 08:26:51 +0100

    Adding alertStatuses to use in many places without break something

---
+++ frontend/src/hooks/useAlerts/alertStatuses.js
@@ -0,0 +1,27 @@
+export default [
+	{
+		type: 'warning',
+		text: 'Pending',
+		status: 'pending'
+	},
+	{
+		type: 'success',
+		text: 'Answered',
+		status: 'answered'
+	},
+	{
+		type: 'danger',
+		text: 'Cancelled',
+		status: 'cancelled'
+	},
+	{
+		type: 'primary',
+		text: 'Completed',
+		status: 'completed'
+	},
+	{
+		type: 'info',
+		text: 'All',
+		status: ''
+	}
+];
